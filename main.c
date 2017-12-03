172393ab72c2d41c99c2b6423d7e30a021622cf9
haikravmax <34074681+haikravmax@users.noreply.github.com>
2017-12-03 16:22:25 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <plib.h>
+
+int main()
+{
+    /*
+     * Set PORTE as output.
+     * Clear PORTE.
+     */
+    TRISE = 0x00;
+    PORTE = 0x00;
+    
+    /*
+     * Set PORTD8 as input.
+     * Clear PORTD.
+     */
+    TRISD |= 1 << 8;
+    PORTD = 0x00;
+    
+    /*
+     *Falling edge.
+     */
+    INITCON &= ~(1 << 1);
+    
+    /*
+     * Set priority.
+     */
+    IPC2 = 1 << 26;
+    
+    /*
+     * Clear flag and enable intetupt.
+     */
+    IFS0 &= ~(1 << 11);
+    IEC0 |= 1 << 11;
+    
+    INTEnableSystemMultiVectoredInt();
+    
+    while(1) ;
+ }
+ 
+ void _ISR(11, ipl6) ChangeNotice_Handler(void)
+ {
+    IFS0 &= ~(1 << 11);
+    PORTE ^= 2;
+ }
